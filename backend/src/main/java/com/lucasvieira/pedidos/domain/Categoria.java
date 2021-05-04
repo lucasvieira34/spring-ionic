ff80e841e0933bb226a29c317b2507da8813787a
Lucas Teixeira <lucas.vieira@brmalls.com.br>
2021-05-03 22:12:21 -0300

    Criando modelo de domínio da Catagoria e BD

---
+++ backend/src/main/java/com/lucasvieira/pedidos/domain/Categoria.java
@@ -0,0 +1,26 @@
+package com.lucasvieira.pedidos.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+}
