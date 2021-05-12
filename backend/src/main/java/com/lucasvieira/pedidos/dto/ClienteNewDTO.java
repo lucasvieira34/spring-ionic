46aadab8f5239315128c8acf5ec6e92595bf606b
Lucas Teixeira <lucas.vieira@brmalls.com.br>
2021-05-12 13:08:11 -0300

    Validação customizada de CPF ou CNPJ na inserção de Cliente

--- backend/src/main/java/com/lucasvieira/pedidos/dto/ClienteNewDTO.java
+++ backend/src/main/java/com/lucasvieira/pedidos/dto/ClienteNewDTO.java
@@ -1,34 +1,42 @@
 package com.lucasvieira.pedidos.dto;
 
+import com.lucasvieira.pedidos.service.validation.ClienteInsert;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ClienteInsert
 public class ClienteNewDTO {
 
+    @NotEmpty(message = "Preenchimento obrigatório.")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres.")
     private String nome;
+    @NotEmpty(message = "Preenchimento obrigatório.")
+    @Email(message = "Email inválido.")
     private String email;
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String cpfOuCnpj;
     private Integer tipo;
-
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String logradouro;
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String numero;
     private String complemento;
     private String bairro;
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String cep;
-
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String telefone1;
     private String telefone2;
     private String telefone3;
 
     private Integer cidadeId;
-
-//    @NotEmpty(message = "Preenchimento obrigatório.")
-//    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres.")
-//    @NotEmpty(message = "Preenchimento obrigatório.")
-//    @Email(message = "Email inválido.")
 
 }
---
+++ backend/src/main/java/com/lucasvieira/pedidos/service/validation/ClienteInsertValidator.java
@@ -0,0 +1,44 @@
+package com.lucasvieira.pedidos.service.validation;
+
+import com.lucasvieira.pedidos.domain.Cliente;
+import com.lucasvieira.pedidos.domain.enumeration.TipoCliente;
+import com.lucasvieira.pedidos.dto.ClienteNewDTO;
+import com.lucasvieira.pedidos.exceptions.FieldMessage;
+import com.lucasvieira.pedidos.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+        }
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
